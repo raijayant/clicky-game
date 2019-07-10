e397e0d3f1c7f08bfbf9c2df34876d3f94abe9c8
Jayant Rai <jayant.chamling@outlook.com>
2019-07-10 11:23:57 -0700

    making code simpler

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,20 +1,103 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import NavBar from './components/Navs/NavBar'
-import Home from './components/Home/Home'
+import FriendCard from './components/FriendCard'
+import NavBar from './components/NavBar'
+import Wrapper from './components/Wrapper'
+import Title from './components/Title'
+import Container from './Container'
+import Row from './Row'
+import Column from './Column'
+import friends from './friends.json'
 import './App.css'
 
+// Random shuffle
+function randomFriends (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+
 class App extends Component {
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    correctIncorrect: '',
+    clicked: []
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement()
+      this.setState({ clicked: this.state.clicked.concat(id) })
+    } else {
+      this.handleReset()
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1
+    this.setState({
+      currentScore: newScore,
+      correctIncorrect: 'You guessed correctly!'
+    })
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore })
+    } else if (newScore === 12) {
+      this.setState({ correctIncorrect: 'You win!' })
+    }
+    this.handleShuffle()
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      correctIncorrect: 'You guessed incorrectly!',
+      clicked: []
+    })
+    this.handleShuffle()
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = randomFriends(friends)
+    this.setState({ friends: shuffledFriends })
+  };
+
   render () {
     return (
-      <Router>
-        <div>
-          <NavBar />
-          <Route exact path='/' component={Home} />
-        </div>
-      </Router>
+      <Wrapper>
+        <NavBar
+          title='Super Mario Clicky Game'
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          correctIncorrect={this.state.correctIncorrect}
+        />
+
+        <Title>
+          Click on an image to earn points, but don't click on any more than once!
+        </Title>
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size='md-3 sm-6'>
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
     )
   }
 }
-
 export default App
---
+++ clicky-game/src/Row.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// const Row = props =>
+//   <div className={`row${props.fluid ? '-fluid' : ''}`}>
+//     {props.children}
+//   </div>
+
+function Row (props) {
+  return (
+    <div className={`row${props.fluid ? '-fluid' : ''}`}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Row
